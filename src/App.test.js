e6fdd8d7493e099b2cb52071955445d745e2c913
Josh Dunson <jdunson98@yahoo.com>
2020-06-11 20:22:10 -0500

    met mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("Form can sumbit", () => {
+  render(<ContactForm />);
+
+  const firstNameInput  = screen.getByTestId(/firstName/i)
+  const lastNameInput = screen.getByTestId(/lastName/i)
+  const emailInput = screen.getByTestId(/email/i)
+  const messageInput = screen.getByTestId(/message/i)
+
+  fireEvent.change(firstNameInput, {
+    target: {value: "First"}
+  });
+  fireEvent.change(lastNameInput, {
+    target: {value: "Last"}
+  });
+  fireEvent.change(emailInput, {
+    target: {value: "email@email.com"}
+  });
+  fireEvent.change(messageInput, {
+    target: {value: "hello"}
+  });
+})
